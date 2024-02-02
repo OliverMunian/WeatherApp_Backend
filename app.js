eeaa45218cbb8dc47a053aedb6682307c63c10e0
Olivier <omalahel@gmail.com>
2024-02-02 13:53:52 +0100

    update appjs

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-require("./models/connection");
-
 var indexRouter = require("./routes/index");
 var weatherRouter = require("./routes/weather");
 var userRouter = require("./routes/users");
